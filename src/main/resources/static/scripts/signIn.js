fd180c3667e00e1f9be435fc9c11c791d25b4898
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2020-02-27 10:12:19 -0600

    Added some validation to employee and password fields

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	// TODO: Anything you want to do when the page is loaded?
+	console.log("hello");
 });
 
 function validateForm() {
 	// TODO: Validate the user input
-	return true;
+	var name = document.getElementById("name").value;
+	var password = document.getElementById("pwd").value;
+	var nameReg = /[0-9]/g;
+	var found = name.match(nameReg);
+	if(name != ""|| password != ""){
+		if(found.length < name.length){
+			alert("Invalid Employee ID!");
+			return false;
+		}
+		return true;
+	}
+	alert("Employee ID or Password is empty!");
+	return false;
 }
