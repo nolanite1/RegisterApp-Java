6bd1f54490854740f418e627885e0400b175ef92
Sailesh Sai Sirigineedi <53832964+nolanite1@users.noreply.github.com>
2020-03-02 19:55:06 -0600

    changing it back

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -13,7 +13,7 @@ import edu.uark.registerapp.controllers.enums.ViewNames;
 @Controller
 @RequestMapping(value = "/signIn")
 public class SignInRouteController extends BaseRouteController {
-	@RequestMapping(method=RequestMethod.GET,value="/")
+	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView start(){
 		return new ModelAndView("signIn");
 	}
